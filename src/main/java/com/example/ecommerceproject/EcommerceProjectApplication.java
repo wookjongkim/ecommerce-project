95e3285a8ea5970f1b6c3521334d096b322c0fe9
Aaron <dnrwhd3@naver.com>
2023-07-19 14:03:28 +0900

    feat : BaseTimeEntity 클래스 추가
    생성일, 수정일이 필요한 엔티티의 경우 해당 클래스 상속받음

--- src/main/java/com/example/ecommerceproject/EcommerceProjectApplication.java
+++ src/main/java/com/example/ecommerceproject/EcommerceProjectApplication.java
@@ -2,8 +2,10 @@ package com.example.ecommerceproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EcommerceProjectApplication {
 
     public static void main(String[] args) {
