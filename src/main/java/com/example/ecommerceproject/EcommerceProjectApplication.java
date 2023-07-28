4efe74418abd48950f21af9f393514bd43d42369
Aaron <dnrwhd3@naver.com>
2023-07-28 09:33:04 +0900

    feat : 판매 중지, 품절 상태를 3년동안 유지한 상품에 대한 배치 삭제 기능 구현
    'SOLD_OUT'이나 'SELL_STOPPED' 상태이며, 3년 동안 업데이트되지 않은 상품에 대한 배치 삭제 기능을 구현
     JpaRepository의 deleteAllInBatch 메소드를 사용하며,
     대량의 데이터를 처리할 때 성능 이슈를 방지하기 위해 상품을
     작은 배치로 나누어 삭제
    트래픽이 가장 적을것으로 예상되는 새벽 3시에 작업 수행

--- src/main/java/com/example/ecommerceproject/EcommerceProjectApplication.java
+++ src/main/java/com/example/ecommerceproject/EcommerceProjectApplication.java
@@ -3,9 +3,11 @@ package com.example.ecommerceproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class EcommerceProjectApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/ecommerceproject/repository/ItemRepository.java
+++ src/main/java/com/example/ecommerceproject/repository/ItemRepository.java
@@ -1,10 +1,15 @@
 package com.example.ecommerceproject.repository;
 
+import com.example.ecommerceproject.constant.ItemSellStatus;
 import com.example.ecommerceproject.domain.model.Item;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface ItemRepository extends JpaRepository<Item, Long>, JpaSpecificationExecutor<Item> {
   Optional<Item> findByIdAndSellerId(long itemId, long sellerId);
+
+  List<Item> findAllBySaleStatusInAndUpdatedAtBefore(List<ItemSellStatus> statuses, LocalDateTime time);
 }
