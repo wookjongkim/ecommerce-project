236dd3dd7d1aac0f4433e9254a8d63eb007c6465
Aaron <dnrwhd3@naver.com>
2023-07-31 19:24:21 +0900

    feat : 구매자의 상품 검색 기능 구현
    구매자가 상품명, 가격 범위, 판매 상태, 카테고리중 일부를 선택하여 상품을 조회하는 기능을 구현하였습니다.가격에 따른 정렬 기능 또한 추가하였습니다.
    
    동적 쿼리 생성을 위해 JPA Specification을 사용하였고, 조회 결과가 많을 경우를 대비하여 페이징 처리 또한 진행하였습니다.

---
+++ src/main/java/com/example/ecommerceproject/domain/dto/BuyerItemResponseDto.java
@@ -0,0 +1,39 @@
+package com.example.ecommerceproject.domain.dto;
+
+import com.example.ecommerceproject.constant.Category;
+import com.example.ecommerceproject.constant.ItemSellStatus;
+import com.example.ecommerceproject.domain.model.Item;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BuyerItemResponseDto {
+
+  private String itemName; // 상품 명
+
+  private String itemDetail; // 상품 설명
+
+  private ItemSellStatus itemSellStatus; // 상품 상태
+
+  private int quantity; // 수량
+
+  private Category category; // 카테고리
+
+  private int price; // 가격
+
+  public static BuyerItemResponseDto of(Item item){
+    return BuyerItemResponseDto.builder()
+        .itemName(item.getItemName())
+        .itemDetail(item.getItemDetail())
+        .itemSellStatus(item.getSaleStatus())
+        .quantity(item.getStock().getQuantity())
+        .category(item.getCateGory())
+        .price(item.getPrice())
+        .build();
+  }
+}
