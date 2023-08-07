2a15213a846cc00d47070c6bca3f63822e077230
Aaron <dnrwhd3@naver.com>
2023-08-07 10:40:50 +0900

    refactor : 상품 수정 url을 PUT에서 PATCH로 수정
    일부 또는 전체 정보의 수정을 모두 지원하기 위해 PATCH 메서드를 채택하였습니다.

--- src/main/java/com/example/ecommerceproject/controller/SellerController.java
+++ src/main/java/com/example/ecommerceproject/controller/SellerController.java
@@ -23,6 +23,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -95,7 +96,7 @@ public class SellerController {
         HttpStatus.OK);
   }
 
-  @PutMapping("/{sellerId}/items/{itemId}")
+  @PatchMapping("/{sellerId}/items/{itemId}")
   public ResponseEntity<ApiResponse> editItem(@PathVariable Long sellerId,
       @PathVariable Long itemId, @Valid ItemUpdateDto itemUpdateDto, BindingResult bindingResult) {
 
